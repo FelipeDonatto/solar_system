0efe93efccdba74192fdc98639f92982697534f5
Felipe <fe.donatto@outlook.com>
2022-06-21 17:00:50 -0300

    5 :ballot_box_with_check:

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
+import PlanetCard from './components/PlanetCard';
+import Planets from './data/planets';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <SolarSystem>
-          <Title> </Title>
-        </SolarSystem>
+        <SolarSystem />
+        <div className="planet-container">
+          { Planets.map((planet) => (
+            <PlanetCard
+              planetName={ planet.name }
+              planetImage={ planet.image }
+              key={ planet.name }
+            />
+          ))}
+        </div>
       </>
     );
   }
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import PropTypes from 'prop-types';
+import React from 'react';
+import Title from './Title';
 
 class PlanetCard extends React.Component {
-    render() {
-        return (
-            
-        )
-    }
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card" className="card">
+        <Title headline={ planetName } />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
 }
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
