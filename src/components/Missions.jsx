81059c23a210b452bece38da4a84102b32dddaab
Felipe <fe.donatto@outlook.com>
2022-06-21 18:54:42 -0300

    :ballot_box_with_check: :ballot_box_with_check:

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import Missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Mission extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className="missionContainer">
         <Title headline="Missões" />
+        {
+          Missions.map((mission) => (<MissionCard
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+            key={ mission.name }
+          />))
+        }
       </div>
     );
   }
